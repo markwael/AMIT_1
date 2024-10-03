60d21c0ec3398650090fe6f0040eea2cc50252d1
mark wael <101711138+markwael@users.noreply.github.com>
2024-10-03 17:32:59 +0300

    task1.py

--- task1.py
+++ task1.py
@@ -1,21 +1,24 @@
 x = "Amit_ml@gmail.edu"
+def con(c):    
+    if '@' and '.' in x:
+        print(True)
+    else:
+        print("Invalid email") 
+    if True:
+        y = x.split('@') 
+        z = x.split('.')
+        print("Extract Username:" , y[0])
+        print("Extract Domain:" ,z[1])
+        
+    if z[1] == "edu":
+        print("Educational Domain")
+    elif z[1] == "com":
+        print("Commercial Domain")
+    else:
+        print("Other Domain")
+     
+print (con(x))
 
-if '@' and '.' in x:
-    print(True)
-else:
-    print("Invalid email") 
-if True:
-    y = x.split('@') 
-    z = x.split('.')
-    print("Extract Username:" , y[0])
-    print("Extract Domain:" ,z[1])
-    
-if z[1] == "edu":
-    print("Educational Domain")
-elif z[1] == "com":
-    print("Commercial Domain")
-else:
-    print("Other Domain")
 '''
 here your output
 True
@@ -24,4 +27,4 @@ Extract Domain: edu
 Educational Domain
 
 '''
-# Great job thank you now i think you can convert this code to be a function 
+# Great job thank you now i think you can convert this code to be a function """"DONE""""
