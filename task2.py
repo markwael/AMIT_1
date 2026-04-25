21167b6d788ca8862244fdf68ca26fbea170213c
username <emailaddress@yahoo.com>
2026-04-25 17:30:16 +0200

    add folder

--- task2.py
+++
@@ -1,9 +0,0 @@
-x = str (input(""))
-y = ''.join([char for char in x if char.isalpha() or char == ' '])
-words = y.split()
-first_word = words[0][::-1]
-second_word = words[1]
-message = f"{first_word} {second_word}"
-print(message) 
-
-#approved---good job
